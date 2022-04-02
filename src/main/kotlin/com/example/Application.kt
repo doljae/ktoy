9965ef1dc1ae1213f90d6d0ea92291bbe501b401
doljae <seok9211@naver.com>
2022-04-02 23:37:12 +0900

    add customer class

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,9 +3,15 @@ package com.example
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.server.application.*
 
 fun main() {
     embeddedServer(Netty, port = 1234, host = "0.0.0.0") {
         configureRouting()
     }.start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,19 +1,15 @@
 package com.example.plugins
 
-import io.ktor.server.routing.*
+import com.example.models.customerStorage
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
 
     // Starting point for a Ktor app:
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-    }
-    routing {
+
     }
 }
